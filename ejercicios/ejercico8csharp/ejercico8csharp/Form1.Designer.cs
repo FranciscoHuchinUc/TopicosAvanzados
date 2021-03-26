c84c80f58e8014c1eaac64bdb53c4469cda0c576
Francisco Huchin <francisco.josue.huchin.uc@gmail.com>
2021-03-26 16:08:07 -0600

    ejercicio8csharp

---
+++ ejercicios/ejercico8csharp/ejercico8csharp/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ejercico8csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.areaTrabajo = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize) (this.areaTrabajo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // areaTrabajo
+            // 
+            this.areaTrabajo.Location = new System.Drawing.Point(-1, 2);
+            this.areaTrabajo.Name = "areaTrabajo";
+            this.areaTrabajo.Size = new System.Drawing.Size(1005, 462);
+            this.areaTrabajo.TabIndex = 0;
+            this.areaTrabajo.TabStop = false;
+            this.areaTrabajo.Paint += new System.Windows.Forms.PaintEventHandler(this.areaTrabajo_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 461);
+            this.Controls.Add(this.areaTrabajo);
+            this.Name = "frmPrincipal";
+            this.Text = "Game";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize) (this.areaTrabajo)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Timer timer1;
+
+        private System.Windows.Forms.PictureBox areaTrabajo;
+
+        #endregion
+    }
+}
---
+++ ejercicios/ejercico8csharp/ejercico8csharp/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ejercico8csharp
+{
+    public partial class Form1 : Form
+    {
+        private Image player = null;
+        private int x, y;
+        private int fx, fy;
+        private int playerMov = 0;
+        private double velociddad = .1;
+        private int tiempo = 200;
+        private bool tipo = false;
+        public Form1()
+        {
+            InitializeComponent();
+            player = Image.FromFile("Recursos\\SpriteCat.png");
+            x = y = 0;
+            fx = fy = 0;
+            tipo = false;
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void areaTrabajo_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(player, 
+                new Rectangle(x,y,80,90), 
+                fx + playerMov * 80, fy,80,90, GraphicsUnit.Pixel);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            playerMov++;
+            if (playerMov >= 3) playerMov = 0;
+            if (tipo) fy = 200;
+            else fy = 0;
+            Console.WriteLine("Move " + playerMov);
+            collisionCheck();
+            areaTrabajo.Invalidate();
+        }
+
+        private void collisionCheck()
+        {
+            //Si llega al limite derecho
+            if (x + 90 >= areaTrabajo.Width)
+            {
+                x = areaTrabajo.Width - 90;
+            }
+
+            if (x <= 0)
+            {
+                x = 0;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine(e.KeyChar.ToString());
+            int distancia = (int) velociddad * tiempo;
+            if (e.KeyChar == 65)
+            {
+                x--;
+                tipo = true;
+            }
+
+            if (e.KeyChar == 68)
+            {
+                x++;
+                tipo = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine("Loberando ");
+            tipo = false;
+        }
+    }
+}
