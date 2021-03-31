4cb482205d612eae29398301891aa539f8ac1b23
Francisco Huchin <francisco.josue.huchin.uc@gmail.com>
2021-03-31 12:42:46 -0600

    practica2

--- practicas/java/practica2/Videojuego.java
+++ practicas/java/practica2/Videojuego.java
@@ -34,22 +34,47 @@ public class Videojuego extends JFrame {
         btnRombo.setFont(new Font("Open Sans", 0, 14));
         btnRombo.setPreferredSize(new Dimension(90,30));
 
+        iconCircle = new JLabel();
+        iconCircle.setIcon(new ImageIcon(getClass().getResource("/practica2/img/imgCircle.png")));
+
+        iconSquare = new JLabel();
+        iconSquare.setIcon(new ImageIcon(getClass().getResource("/practica2/img/imgSquare.png")));
+
+        iconRectangle = new JLabel();
+        iconRectangle.setIcon(new ImageIcon(getClass().getResource("/practica2/img/imgRectangle.png")));
+
+        iconTriangle = new JLabel();
+        iconTriangle.setIcon(new ImageIcon(getClass().getResource("/practica2/img/imgTriangle.png")));
+
         barraBoton = new JPanel();
+        barraBoton.setBackground(new Color(216, 241, 255));
         barraBoton.add(btnCirculo);
         barraBoton.add(btnCuadrado);
         barraBoton.add(btnRombo);
 
-        this.add(barraBoton, BorderLayout.EAST);
-        this.add(contDibujo, BorderLayout.CENTER);
+        scrollPane = new JScrollPane();
+        scrollPane.setBorder(null);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setFocusTraversalPolicyProvider(true);
+        contOpciones = new JPanel();
+        contOpciones.setPreferredSize(new Dimension(90, 400));
+        contOpciones.setBackground(new Color(216, 241, 255));
+        contOpciones.add(iconCircle);
+        contOpciones.add(iconSquare);
+        contOpciones.add(iconRectangle);
+        contOpciones.add(iconTriangle);
+        scrollPane.setViewportView(contOpciones);
+
+        this.add(barraBoton, BorderLayout.NORTH);
+        this.add(scrollPane, BorderLayout.EAST);
 
         //Personalizacion de la ventana
         this.setSize(500, 500);
         this.setLocationRelativeTo(null);
-        this.setResizable(false);
+        //this.setResizable(false);
         this.setVisible(true);
 
     }
-
     
     public static void main(String[] args) {
         Videojuego videojuego = new Videojuego();
@@ -64,9 +89,14 @@ public class Videojuego extends JFrame {
     JButton btnCirculo;
     JButton btnRombo;
     JButton btnCuadrado;
+    JLabel iconCircle;
+    JLabel iconSquare;
+    JLabel iconRectangle;
+    JLabel iconTriangle;
     JPanel barraBoton;
     JPanel contDibujo;
     JPanel contOpciones;
+    JScrollPane scrollPane;
     // End of variables declaration
     
 }
