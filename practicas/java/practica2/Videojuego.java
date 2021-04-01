e9fa416e90040a2b3be603eceb67445de08942f6
Francisco Huchin <francisco.josue.huchin.uc@gmail.com>
2021-03-31 23:37:43 -0600

    Practica2

--- practicas/java/practica2/Videojuego.java
+++ practicas/java/practica2/Videojuego.java
@@ -10,29 +10,44 @@ public class Videojuego extends JFrame {
      *
      */
     private static final long serialVersionUID = 1L;
-    
-    public Videojuego(){
+
+    Figura oFigura;
+
+    public Videojuego() {
         initComponets();
     }
 
-    public void initComponets(){
+    public void initComponets() {
 
         this.setLayout(new BorderLayout());
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                formMouseClicked(e);
+            }
+        });
+        this.addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                formMouseDragged(e);
+            }
+        });
+
+        oFigura = new Figura();
 
         btnCirculo = new JButton("Circulo");
         btnCirculo.setFocusable(false);
         btnCirculo.setFont(new Font("Open Sans", 0, 14));
-        btnCirculo.setPreferredSize(new Dimension(90,30));
+        btnCirculo.setPreferredSize(new Dimension(90, 30));
+        btnCirculo.addActionListener(e -> btnCirculoActionPerformed(e));
 
         btnCuadrado = new JButton("Cuadrado");
         btnCuadrado.setFocusable(false);
         btnCuadrado.setFont(new Font("Open Sans", 0, 14));
-        btnCuadrado.setPreferredSize(new Dimension(110,30));
+        btnCuadrado.setPreferredSize(new Dimension(110, 30));
 
         btnRombo = new JButton("Rombo");
         btnRombo.setFocusable(false);
         btnRombo.setFont(new Font("Open Sans", 0, 14));
-        btnRombo.setPreferredSize(new Dimension(90,30));
+        btnRombo.setPreferredSize(new Dimension(90, 30));
 
         iconCircle = new JLabel();
         iconCircle.setIcon(new ImageIcon(getClass().getResource("/practica2/img/imgCircle.png")));
@@ -67,15 +82,16 @@ public class Videojuego extends JFrame {
 
         this.add(barraBoton, BorderLayout.NORTH);
         this.add(scrollPane, BorderLayout.EAST);
+        this.add(oFigura, BorderLayout.CENTER);
 
         //Personalizacion de la ventana
         this.setSize(500, 500);
         this.setLocationRelativeTo(null);
-        //this.setResizable(false);
+        this.setResizable(false);
         this.setVisible(true);
 
     }
-    
+
     public static void main(String[] args) {
         Videojuego videojuego = new Videojuego();
         videojuego.addWindowListener(new WindowAdapter() {
@@ -83,6 +99,20 @@ public class Videojuego extends JFrame {
                 System.exit(0);
             }
         });
+    }
+
+    private void btnCirculoActionPerformed(ActionEvent e) {
+        oFigura.setIsFigura("Circulo");
+        JOptionPane.showMessageDialog(null, "Circulo");
+    }
+
+    private void formMouseClicked(MouseEvent e) {
+        oFigura.repaint();
+        btnCirculo.setText("Clicked");
+    }
+
+    private void formMouseDragged(java.awt.event.MouseEvent evt) {
+        System.out.println("Move");
     }
 
     // Variables declaration
@@ -98,5 +128,5 @@ public class Videojuego extends JFrame {
     JPanel contOpciones;
     JScrollPane scrollPane;
     // End of variables declaration
-    
+
 }
