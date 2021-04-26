84c746d8fa5ea36ee5cf0f8ebcf859ff9b42314f
Francisco Huchin <francisco.josue.huchin.uc@gmail.com>
2021-04-26 11:16:52 -0500

    practica3

--- practicas/java/practica3/Main.java
+++ practicas/java/practica3/Main.java
@@ -1,27 +1,10 @@
 package practica3;
 
-import java.util.Arrays;
-
-import practica3.directorio.Directorios;
-import practica3.ip.DireccionIp;
-import practica3.tiempo.TiempoCarga;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        String host1 = "itescam.edu.mx";
-        String host = "https://itescam.edu.mx";
-
-        DireccionIp ip = new DireccionIp();
-        System.out.println(ip.direccion(host1));
-        
-        TiempoCarga t = new TiempoCarga();
-        System.out.println(t.carga(host));
-        
-        Directorios dir = new Directorios();
-        System.out.println(Arrays.toString(dir.directoriosExtistentes(host)));
-        System.out.println(dir.getTam());
+        UserInterface = new UserInterface();
     }
     
 }
--- practicas/java/practica3/tiempo/TiempoCarga.java
+++ practicas/java/practica3/tiempo/TiempoCarga.java
@@ -6,10 +6,11 @@ public class TiempoCarga {
     public int carga(String host){
         try {
             Socket socket = new Socket(host, 80);
+            socket.setSoTimeout(60);
             int time = socket.getSoTimeout();
             return time;
         } catch (Exception e) {
         }
         return 0;
-    }    
+    }
 }
