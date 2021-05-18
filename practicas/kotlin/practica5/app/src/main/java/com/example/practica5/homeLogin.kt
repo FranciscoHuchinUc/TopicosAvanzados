3ffcb3e8cce1726d4814bd139aa5f30a0f024030
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2021-05-17 21:35:27 -0500

    practica5

--- practicas/kotlin/practica5/app/src/main/java/com/example/practica5/homeLogin.kt
+++ practicas/kotlin/practica5/app/src/main/java/com/example/practica5/homeLogin.kt
@@ -3,8 +3,11 @@ package com.example.practica5
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import androidx.core.text.set
 
 class homeLogin : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_login)
