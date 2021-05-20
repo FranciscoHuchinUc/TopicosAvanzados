0ac06f7d016eecee5a8cf86a612c180273a0bc1e
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2021-05-19 23:24:31 -0500

    practica6

---
+++ practicas/kotlin/practica6/app/src/main/java/com/example/practica6/componentes/Game.kt
@@ -0,0 +1,4 @@
+package com.example.practica6.componentes
+
+class Game {
+}
---
+++ practicas/kotlin/practica6/app/src/main/java/com/example/practica6/interfaz/GameStateListener.kt
@@ -0,0 +1,5 @@
+package com.example.practica6.interfaz
+
+interface GameStateListener {
+    fun stateChanged(state: Game.ESTATE)
+}
