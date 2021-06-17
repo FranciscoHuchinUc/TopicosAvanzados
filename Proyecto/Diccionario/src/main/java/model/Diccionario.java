ddb37ae721a72fad1da691541589677ac2d033f2
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2021-06-17 00:30:54 -0500

    Diccionario

--- Proyecto/Diccionario/src/main/java/model/Diccionario.java
+++ Proyecto/Diccionario/src/main/java/model/Diccionario.java
@@ -5,31 +5,32 @@ import com.google.firebase.database.IgnoreExtraProperties;
 @IgnoreExtraProperties
 public class Diccionario {
 
-    public String Palabra;
-    public String Significado;
+    public String palabra;
+    public String significado;
 
     public Diccionario() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public Diccionario(String Palabra, String Significado) {
-        this.Palabra = Palabra;
-        this.Significado = Significado;
+    public Diccionario(String palabra, String significado) {
+        this.palabra = palabra;
+        this.significado = significado;
     }
 
     public String getPalabra() {
-        return Palabra;
+        return palabra;
     }
 
     public void setPalabra(String palabra) {
-        Palabra = palabra;
+        this.palabra = palabra;
     }
 
     public String getSignificado() {
-        return Significado;
+        return significado;
     }
 
     public void setSignificado(String significado) {
-        Significado = significado;
+        this.significado = significado;
     }
+    
 }
